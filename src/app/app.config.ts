81ef2ec6d855051ebac851fe514da40d21ca5cf5
paulpladziewicz <ppladziewicz@gmail.com>
2025-04-19 06:00:31 -0400

    using the latest ngx-ui components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -12,14 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: 'none'
-        }
-      }
-    }),
     provideHttpClient()
   ]
 };
